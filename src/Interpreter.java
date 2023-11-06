5ba5577c5185ba103c251e7f5c39c131092dcaec
Denis <pop.denis.v@gmail.com>
2023-11-06 10:14:24 +0200

    separate interpreters

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,7 +1,11 @@
 import view.cli.CliInterpreter;
+import view.gui.GuiInterpreter;
 
 public class Interpreter {
     public static void main(String[] args) {
-        CliInterpreter.main(args);
+        if (args.length == 0) {
+            CliInterpreter.main(args);
+//            GuiInterpreter.main(args);
+        }
     }
 }
